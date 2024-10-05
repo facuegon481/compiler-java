cc9804e0f32c4aa2beb76e364e92f999eec5de62
MiguelAlbarracin <miguel.albarracin@whiteprompt.com>
2024-10-04 22:41:15 -0300

    Tercetos 1.0

--- src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
+++ src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
@@ -2,11 +2,35 @@ package lyc.compiler.files;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+
+import lyc.compiler.model.Terceto;
 
 public class IntermediateCodeGenerator implements FileGenerator {
+    private static IntermediateCodeGenerator intermediateCodeGenerator;
+    private static HashMap<Integer,Terceto> ListaTercetos = new HashMap<Integer,Terceto>();
+    private static Integer NuevoTercetoNum = 11;
+
+    public static IntermediateCodeGenerator getInstance() {
+        if(intermediateCodeGenerator == null) {
+            intermediateCodeGenerator = new IntermediateCodeGenerator();
+        }
+        return intermediateCodeGenerator;
+    }
 
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
-        fileWriter.write("TODO");
+        String file = "";
+        for (Integer key : ListaTercetos.keySet()) {
+            file += "[" + key.toString() + "] "+ ListaTercetos.get(key).toString() + "\n";
+        }
+        fileWriter.write(file);
+    }
+
+    public static Integer CrearTerceto(String primero, String segundo, String tercero) {
+        Terceto nuevo = new Terceto(primero, segundo, tercero);
+        ListaTercetos.put(NuevoTercetoNum, nuevo);
+        NuevoTercetoNum++;
+        return NuevoTercetoNum - 1;
     }
 }
---
+++ src/main/java/lyc/compiler/model/Terceto.java
@@ -0,0 +1,42 @@
+package lyc.compiler.model;
+
+public class Terceto {
+    private String first;
+    private String second;
+    private String third;
+
+    public Terceto(String first, String second, String third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+
+    public String getThird() {
+        return third;
+    }
+
+    public void setThird(String third) {
+        this.third = third;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ", " + third + ")";
+    }
+}
